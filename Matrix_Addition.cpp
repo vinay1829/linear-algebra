b8bb7dd43c45d70e6d98204999143c3c888f4178
Vinay <146519041+vinay1829@users.noreply.github.com>
2023-12-19 18:28:08 +0530

    Matrix Operation
    Updated operator overloading and consistent with different dimensions of matrices.

--- Matrix_Addition.cpp
+++ Matrix_Addition.cpp
@@ -2,62 +2,59 @@
 #include <vector>
 #include <CL/cl2.hpp>
 using namespace std;
-// Square Matrix of size n*n.
 
 class Matrix
 {
-    // INPUT AND OUTPUT MATRICES.
-    vector<int> firstMatrix;
-    vector<int> secondMatrix;
-    vector<int> ResultantMatrix;
-   
+    int n;
+    int m;
+    vector<int> values;
     // OpenCL kernel code for matrix addition
     const char *kernelSource = R"(
     __kernel void matrix_addition(
         __global int* A,
         __global int* B,
         __global int* C,
-        int N)
+        int N,
+        int M)
     {
         int row = get_global_id(0);
         int col = get_global_id(1);
-
-        C[row * N + col] = A[row*N+col]+B[row*N+col];
+        C[row*M + col] = A[row*M + col] + B[row*M + col];
     }
     )";
 
 public:
-    Matrix(int n)
+    Matrix(int row, int col)
     {
-        firstMatrix.resize(n * n);
-        secondMatrix.resize(n * n);
-        ResultantMatrix.resize(n * n);
+        values.resize(row * col, 1e9);
+        n = row;
+        m = col;
     }
 
-    void getValueFirstMatrix(int n)
+    Matrix(int input[], int row, int col)
     {
-        for (int i = 0; i < n; ++i)
+        values.resize(row * col);
+        n = row;
+        m = col;
+        for (int i = 0; i < row * col; ++i)
         {
-            for (int j = 0; j < n; ++j)
-            {
-                cin >> firstMatrix[i * n + j];
-            }
+            values[i] = input[i];
         }
     }
 
-    void getValueSecondMatrix(int n)
+    Matrix operator+(Matrix &firstMatrix)
     {
-        for (int i = 0; i < n; ++i)
+        int row = firstMatrix.n;
+        int col = firstMatrix.m;
+        int other_row = this->n;
+        int other_col = this->m;
+        Matrix ResultantMatrix(row, col);
+        if (row != other_row || col != other_col)
         {
-            for (int j = 0; j < n; ++j)
-            {
-                cin >> secondMatrix[i * n + j];
-            }
+            cout << "Wrong Dimensions\n";
+            return ResultantMatrix;
         }
-    }
 
-    void matrixAddition(int n)
-    {
         cl::Context context(CL_DEVICE_TYPE_GPU);
 
         // Get the devices associated with the context
@@ -75,24 +72,25 @@ public:
         cl::Kernel kernel(program, "matrix_addition");
 
         // Create OpenCL buffers for matrices A, B, and C
-        cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, n * n * sizeof(int), firstMatrix.data());
-        cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, n * n * sizeof(int), secondMatrix.data());
-        cl::Buffer bufferC(context, CL_MEM_WRITE_ONLY, n * n * sizeof(int));
+        cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, row * col * sizeof(int), this->values.data());
+        cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, row * col * sizeof(int), firstMatrix.values.data());
+        cl::Buffer bufferC(context, CL_MEM_WRITE_ONLY, row * col * sizeof(int));
 
         // Set OpenCL kernel arguments
         kernel.setArg(0, bufferA);
         kernel.setArg(1, bufferB);
         kernel.setArg(2, bufferC);
-        kernel.setArg(3, n);
+        kernel.setArg(3, row);
+        kernel.setArg(4, col);
 
         // Execute the OpenCL kernel
-        cl::NDRange globalWorkSize(n, n);
+        cl::NDRange globalWorkSize(row, col);
         cl::Event event;
         queue.enqueueNDRangeKernel(kernel, cl::NullRange, globalWorkSize, cl::NullRange, NULL, &event);
         event.wait();
 
         // Read the result from the OpenCL buffer
-        queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, n * n * sizeof(int), ResultantMatrix.data());
+        queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, row * col * sizeof(int), ResultantMatrix.values.data());
 
         // Profiling information
         cl_ulong start_time, end_time;
@@ -103,16 +101,20 @@ public:
         cout << "Start time: " << start_time << " ns" << endl;
         cout << "End time: " << end_time << " ns" << endl;
         cout << "Execution time: " << execution_time << " ms" << endl;
+
+        return ResultantMatrix;
     }
 
-    void printResult(int n)
+    void printResult(int row, int col)
     {
-    	cout<<"\nResultant Matrix:\n";
-        for (int i = 0; i < n; ++i)
+        if (this->values[0] == 1e9)
+            return;
+        cout << "\nResultant Matrix:\n";
+        for (int i = 0; i < row; ++i)
         {
-            for (int j = 0; j < n; ++j)
+            for (int j = 0; j < col; ++j)
             {
-                cout << ResultantMatrix[i * n + j] << " ";
+                cout << this->values[j + i * col] << " ";
             }
             cout << endl;
         }
@@ -121,12 +123,21 @@ public:
 
 int main()
 {
-    int n;
-    cin >> n;
-    Matrix mat(n);
-    mat.getValueFirstMatrix(n);
-    mat.getValueSecondMatrix(n);
-    mat.matrixAddition(n);
-    mat.printResult(n);
+    int row1 = 3;
+    int col1 = 4;
+    int row2 = 3;
+    int col2 = 4;
+    //////////////////////////
+    int a[row1 * col1] = {1, 1, 1, 1,
+                          1, 1, 1, 1,
+                          1, 1, 1, 1};
+    /////////////////////////
+    int b[row2 * col2] = {2, 2, 2, 2,
+                          2, 2, 2, 2,
+                          2, 2, 2, 2};
+    Matrix mat1(a, row1, col1);
+    Matrix mat2(b, row2, col2);
+    Matrix mat3 = mat1 + mat2;
+    mat3.printResult(row1, col1);
     return 0;
 }
--- Matrix_Multiplication.cpp
+++ Matrix_Multiplication.cpp
@@ -2,67 +2,65 @@
 #include <vector>
 #include <CL/cl2.hpp>
 using namespace std;
-// Square Matrix of size n*n.
 
 class Matrix
 {
-    // INPUT AND OUTPUT MATRICES.
-    vector<int> firstMatrix;
-    vector<int> secondMatrix;
-    vector<int> ResultantMatrix;
-
+    int n;
+    int m;
+    vector<int> values;
     // OpenCL kernel code for matrix multiplication
     const char *kernelSource = R"(
     __kernel void matrix_multiply(
         __global int* A,
         __global int* B,
         __global int* C,
-        int N)
+        int P,
+        int R)
     {
         int row = get_global_id(0);
         int col = get_global_id(1);
 
         int sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += A[row * N + i] * B[i * N + col];
+        for (int i = 0; i < P; i++) {
+            sum += A[row * P + i] * B[i * R + col];
         }
 
-        C[row * N + col] = sum;
+        C[row * R + col] = sum;
     }
     )";
 
 public:
-    Matrix(int n)
+    Matrix(int row, int col)
     {
-        firstMatrix.resize(n * n);
-        secondMatrix.resize(n * n);
-        ResultantMatrix.resize(n * n);
+        values.resize(row * col, 1e9);
+        n = row;
+        m = col;
     }
 
-    void getValueFirstMatrix(int n)
+    Matrix(int input[], int row, int col)
     {
-        for (int i = 0; i < n; ++i)
+        values.resize(row * col);
+        n = row;
+        m = col;
+        for (int i = 0; i < row * col; ++i)
         {
-            for (int j = 0; j < n; ++j)
-            {
-                cin >> firstMatrix[i * n + j];
-            }
+            values[i] = input[i];
         }
     }
 
-    void getValueSecondMatrix(int n)
+    Matrix operator*(Matrix &secondMatrix)
     {
-        for (int i = 0; i < n; ++i)
+        int p = secondMatrix.n;         // row2
+        int q1 = secondMatrix.m;        // col2
+        int q2 = this->n;               // row1
+        int r = this->m;                // col1
+        Matrix ResultantMatrix(q2, q1); // Output matrix dimension will be row1 x col2
+        if (p != r)
         {
-            for (int j = 0; j < n; ++j)
-            {
-                cin >> secondMatrix[i * n + j];
-            }
+            cout << "Dimensions are not correct\n";
+            return ResultantMatrix;
         }
-    }
 
-    void matrixMultiplication(int n)
-    {
         cl::Context context(CL_DEVICE_TYPE_GPU);
 
         // Get the devices associated with the context
@@ -80,24 +78,25 @@ public:
         cl::Kernel kernel(program, "matrix_multiply");
 
         // Create OpenCL buffers for matrices A, B, and C
-        cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, n * n * sizeof(int), firstMatrix.data());
-        cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, n * n * sizeof(int), secondMatrix.data());
-        cl::Buffer bufferC(context, CL_MEM_WRITE_ONLY, n * n * sizeof(int));
+        cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, q2 * r * sizeof(int), this->values.data());
+        cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, p * q1 * sizeof(int), secondMatrix.values.data());
+        cl::Buffer bufferC(context, CL_MEM_WRITE_ONLY, q1 * q2 * sizeof(int));
 
         // Set OpenCL kernel arguments
         kernel.setArg(0, bufferA);
         kernel.setArg(1, bufferB);
         kernel.setArg(2, bufferC);
-        kernel.setArg(3, n);
+        kernel.setArg(3, p);
+        kernel.setArg(4, q1);
 
         // Execute the OpenCL kernel
-        cl::NDRange globalWorkSize(n, n);
+        cl::NDRange globalWorkSize(q2, q1);
         cl::Event event;
         queue.enqueueNDRangeKernel(kernel, cl::NullRange, globalWorkSize, cl::NullRange, NULL, &event);
         event.wait();
 
         // Read the result from the OpenCL buffer
-        queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, n * n * sizeof(int), ResultantMatrix.data());
+        queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, q1 * q2 * sizeof(int), ResultantMatrix.values.data());
 
         // Profiling information
         cl_ulong start_time, end_time;
@@ -108,16 +107,20 @@ public:
         cout << "Start time: " << start_time << " ns" << endl;
         cout << "End time: " << end_time << " ns" << endl;
         cout << "Execution time: " << execution_time << " ms" << endl;
+
+        return ResultantMatrix;
     }
 
-    void printResult(int n)
+    void printResult(int row, int col)
     {
+        if (this->values[0] == 1e9)
+            return;
         cout << "\nResultant Matrix:\n";
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < row; ++i)
         {
-            for (int j = 0; j < n; ++j)
+            for (int j = 0; j < col; ++j)
             {
-                cout << ResultantMatrix[i * n + j] << " ";
+                cout << this->values[j + i * col] << " ";
             }
             cout << endl;
         }
@@ -126,12 +129,21 @@ public:
 
 int main()
 {
-    int n;
-    cin >> n;
-    Matrix mat(n);
-    mat.getValueFirstMatrix(n);
-    mat.getValueSecondMatrix(n);
-    mat.matrixMultiplication(n);
-    mat.printResult(n);
+    int row1 = 2;
+    int col1 = 3;
+    int row2 = 3;
+    int col2 = 4;
+    //////////////////////////
+    int a[row1 * col1] = {2, 1, 4,
+                          0, 1, 1};
+    /////////////////////////
+    int b[row2 * col2] = {6, 3, -1, 0,
+                          1, 1, 0, 4,
+                          -2, 5, 0, 2};
+    Matrix mat1(a, row1, col1);
+    Matrix mat2(b, row2, col2);
+    Matrix mat3 = mat1 * mat2;
+    // Size of output matrix will be row1 and col2.
+    mat3.printResult(row1, col2);
     return 0;
 }
